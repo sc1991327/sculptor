dada26b5bc35acf6740f2ad44c94070046fa48bf
sc1991327 <sc1991327@gmail.com>
2016-05-19 16:37:39 +0800

    重构手柄模型及使用，网络模式头部模型加入

---
+++ Assets/Sculptor/HandManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandManager : MonoBehaviour {
+
+    public GameObject HandObject;
+
+    public GameObject ModelCreateObject;
+    public GameObject ModelDeleteObject;
+    public GameObject ModelSmoothObject;
+    public GameObject ModelPaintObject;
+
+    private HandBehaviour handBehaviour;
+    private OptState activeState = OptState.create;
+
+    // Use this for initialization
+    void Start () {
+
+        handBehaviour = HandObject.GetComponent<HandBehaviour>();
+
+        ModelCreateObject.SetActive(true);
+        ModelDeleteObject.SetActive(false);
+        ModelSmoothObject.SetActive(false);
+        ModelPaintObject.SetActive(false);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        OptState tempState = handBehaviour.GetActiveState();
+        if (tempState != activeState)
+        {
+            switch (tempState)
+            {
+                case OptState.create:
+                    ModelCreateObject.SetActive(true);
+                    ModelDeleteObject.SetActive(false);
+                    ModelSmoothObject.SetActive(false);
+                    ModelPaintObject.SetActive(false);
+                    break;
+
+                case OptState.delete:
+                    ModelCreateObject.SetActive(false);
+                    ModelDeleteObject.SetActive(true);
+                    ModelSmoothObject.SetActive(false);
+                    ModelPaintObject.SetActive(false);
+                    break;
+
+                case OptState.smooth:
+                    ModelCreateObject.SetActive(false);
+                    ModelDeleteObject.SetActive(false);
+                    ModelSmoothObject.SetActive(true);
+                    ModelPaintObject.SetActive(false);
+                    break;
+
+                case OptState.paint:
+                    ModelCreateObject.SetActive(false);
+                    ModelDeleteObject.SetActive(false);
+                    ModelSmoothObject.SetActive(false);
+                    ModelPaintObject.SetActive(true);
+                    break;
+            }
+            activeState = tempState;
+        }
+
+	}
+}
