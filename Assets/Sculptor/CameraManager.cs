25c6f8a829a55cf2cabee8cb557f2512ec7e6b2f
sc1991327 <sc1991327@gmail.com>
2016-06-29 10:40:47 +0800

    Vive调整操作区域大小方式修改

--- Assets/Sculptor/CameraManager.cs
+++ Assets/Sculptor/CameraManager.cs
@@ -377,21 +377,33 @@ public class CameraManager : MonoBehaviour {
                 else
                 {
                     // Detect top and bottom swipe
-                    angleOfSwipe = Vector2.Dot(swipeVector, mYAxis);
-                    angleOfSwipe = Mathf.Acos(angleOfSwipe) * Mathf.Rad2Deg;
-                    if (angleOfSwipe < mAngleRange)
-                    {
-                        OnSwipeTop();
-                        return 4;
-                    }
-                    else if ((180.0f - angleOfSwipe) < mAngleRange)
-                    {
-                        OnSwipeBottom();
-                        return 3;
-                    }
+                    //angleOfSwipe = Vector2.Dot(swipeVector, mYAxis);
+                    //angleOfSwipe = Mathf.Acos(angleOfSwipe) * Mathf.Rad2Deg;
+                    //if (angleOfSwipe < mAngleRange)
+                    //{
+                    //    OnSwipeTop();
+                    //    return 4;
+                    //}
+                    //else if ((180.0f - angleOfSwipe) < mAngleRange)
+                    //{
+                    //    OnSwipeBottom();
+                    //    return 3;
+                    //}
                 }
             }
-
+            else
+            {
+                if (mEndPosition.y > 0.2)
+                {
+                    OnSwipeTop();
+                    return 4;
+                }
+                else if (mEndPosition.y < -0.2)
+                {
+                    OnSwipeBottom();
+                    return 3;
+                }
+            }
         }
 
         return -1;
